cf6ffec517ba06ca6c69840185c39ff0dadaf020
K_Kwon <ts04031@gmail.com>
2022-02-04 20:03:11 +0900

    숫자의 입력과 .(dot)이 입력 구현 완료

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -4,15 +4,34 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private var tvInput : TextView? = null
+    var textNum : Boolean = false // 첨에는 숫자가 없음
+    var lastDot : Boolean = false // 첨에는 dot도 없음
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tvInput = findViewById(R.id.tvinput)
     }
 
-    fun onDigit(view : View) {
-        Toast.makeText(this, "btn clicked", Toast.LENGTH_LONG).show()
+    fun onDigit(view : View) { // view는 실제 버튼을 의미
+        tvInput?.append((view as Button).text)
+        textNum = true // 숫자가 입력되면 textNum은 ture로 변환
+    }
+
+    fun onClick(view: View) {
+        tvInput?.text = ""
+    }
+
+    fun onDecimalPoint(view : View) {
+        if (textNum == true && lastDot == false) {
+            tvInput?.append(".")
+            lastDot = true // dot이 존재하는지 저장하는 변수
+        }
     }
 }
