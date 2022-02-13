7e18347e79c795e6d20909a24ed0680f30045731
K_Kwon <ts04031@gmail.com>
2022-02-14 02:49:57 +0900

    Onoperator 기능 추가

--- app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
     private var tvInput : TextView? = null
     var textNum : Boolean = false // 첨에는 숫자가 없음
     var lastDot : Boolean = false // 첨에는 dot도 없음
+    var lastNumeric : Boolean = false // 마지막이 숫자인지 확인
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,6 +23,7 @@ class MainActivity : AppCompatActivity() {
     fun onDigit(view : View) { // view는 실제 버튼을 의미
         tvInput?.append((view as Button).text)
         textNum = true // 숫자가 입력되면 textNum은 ture로 변환
+        lastNumeric = true // 마지막이 숫자
     }
 
     fun onClick(view: View) {
@@ -29,9 +31,31 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDecimalPoint(view : View) {
-        if (textNum == true && lastDot == false) {
+        if (textNum && !lastDot && lastNumeric) {
             tvInput?.append(".")
             lastDot = true // dot이 존재하는지 저장하는 변수
+            lastNumeric = false // 마지막이 dot이 됨
+        }
+    }
+
+    fun onOperator(view : View) {
+        tvInput?.text?.let {
+            if(lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false
+                lastDot = false
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value : String) : Boolean {
+        return if (value.startsWith("-")) {
+            false // -기호를 무시하기 위함
+        } else {
+            value.contains("/")
+                    || value.contains("*")
+                    || value.contains("+")
+                    || value.contains("-")
         }
     }
 }
